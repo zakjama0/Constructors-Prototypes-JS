285aad6f4c51ff0de7e5ed88c622405edc187b71
Zakaria <zakjama0@gmail.com>
2024-03-13 14:03:47 +0000

    Mapping and Filtering Revision

--- high_order_functions/enumeration.js
+++ high_order_functions/enumeration.js
@@ -1,5 +1,30 @@
 const numbers = [1,2,3,4,5];
 
-for (let i = 0; i<numbers.length; i++){
-    console.log(numbers[i]);
+
+const printElement = (arrayElement) =>{
+    console.log(arrayElement);
 }
+// call back gets called for each an element in an array
+
+numbers.forEach(printElement);
+
+
+console.log("numbers:",numbers);
+
+// MAPPING
+const doubleNumbers = numbers.map((number) =>{
+return number*2
+})
+console.log("doubleNumbers:", doubleNumbers);
+
+//FILTERING
+
+// has to be a boolean
+const evenNumbers = numbers.filter((number) =>{
+    return number % 2 == 0;
+})
+
+console.log("evenNumbers:", evenNumbers);
+
+// Reduce Method
+
